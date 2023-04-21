8e7d1413c6b6ab17aa7adce6e9ecdd70ee235e77
Patrick Thomson <patrickt@github.com>
2023-04-21 14:18:13 -0400

    Initial Commit

---
+++ chopwatcher/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  chopwatcher
+//
+//  Created by Patrick Thomson on 4/21/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
